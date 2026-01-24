48c2c6799576f576b776408d271f3f171c109354
vboxuser <alliah.brown@ensg.eu>
2026-01-24 20:38:00 +0000

    first commit

---
+++ src/controller/delete.ts
@@ -0,0 +1,17 @@
+import { VehicleStore } from '../store/vehicle';
+import { Request, Response } from 'express';
+
+interface Parameters {
+  id: string;
+}
+
+export class DeleteVehicleController {
+  constructor(private readonly vehicleStore: VehicleStore) {}
+
+  public async handle(req: Request<Parameters>, res: Response): Promise<void> {
+    await this.vehicleStore.deleteVehicle({id: parseInt(req.params.id)});
+    res.status(204).send();
+  }
+}
+
+
