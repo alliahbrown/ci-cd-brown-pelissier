341f9f1f1944606915607cef4e106e025adeb4e5
vboxuser <alliah.brown@ensg.eu>
2026-01-28 21:09:45 +0000

    ajout lint

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+// ./eslint.config.mjs
+// @ts-check
+
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+);
